4c19d8e32852cd59594b16e765b330d8a501c737
Rylan Cole <rylancole@Rylans-Air.hitronhub.home>
2020-12-16 16:25:52 -0800

    Ignore .py files

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import os
+import json
 
 def main():
-  print("Hello World")
+  countries = [country[:-5] for country in os.listdir('./json')]
+  
+  for country in countries:
+    print('\"'+country+'\",')
 
 if __name__ == "__main__":
   main()
